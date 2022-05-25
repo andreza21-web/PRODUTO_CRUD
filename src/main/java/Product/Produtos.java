032821014a7884fbef53df872d3d575f19be7183
andreza <andreza.21pessoa@gmail.com>
2022-05-25 19:27:37 -0300

    atividade tp2

---
+++ src/main/java/Product/Produtos.java
@@ -0,0 +1,50 @@
+package Product;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+
+public class Produtos {
+
+	private static List<Produto> lista = new ArrayList<Produto>();
+	private static Integer chaveSequencial = 1;	
+
+	public List<Produto> listarProdutos() {
+		return this.lista;
+	}
+	
+	public void adiciona(Produto produto) {
+		adicionarProduto(produto);
+	}
+
+	private static void adicionarProduto(Produto produto) {
+		produto.setId(chaveSequencial++);
+		lista.add(produto);
+	}
+	
+	public void remover(long id) {
+		Iterator<Produto> iProdutos = lista.iterator();
+		
+		while(iProdutos.hasNext()) {
+			Produto produto = iProdutos.next();
+			
+			if(produto.getId() == id) {
+				iProdutos.remove();
+			}
+		}
+	}
+	
+	public Produto buscarProduto(long id) {
+		for(Produto produto : lista) {
+			if(produto.getId() == id) {
+				return produto;
+			}
+		}
+		
+		return null;
+	}
+
+
+}
---
+++ src/main/java/servlets/RemoveProdutoServlet.java
@@ -0,0 +1,30 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Product.Produtos;
+
+@WebServlet("/RemoveProduto")
+public class RemoveProdutoServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		long id = Long.parseLong(req.getParameter("id"));
+
+		Produtos produto = new Produtos();
+
+		produto.remover(id);
+
+		resp.sendRedirect("listarProduto");
+	}
+
+}
